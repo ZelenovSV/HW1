1513a9e9dc957205589bac3b83a79556519c626c
ZelenovSV <130816124+ZelenovSV@users.noreply.github.com>
2023-04-18 23:26:05 +0300

    Add files via upload

---
+++ Задача 8.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число :");
+int  s = int.Parse(Console.ReadLine());
+for (int i = 1; i < s; i++)
+{
+    if ((i) % 2==0)
+    {
+    Console.Write(i + " ");
+    }
+}
+    
