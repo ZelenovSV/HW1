d50d3c1977ef4020ee6484cb4bebff6fb077f6ec
ZelenovSV <130816124+ZelenovSV@users.noreply.github.com>
2023-04-18 23:25:50 +0300

    Add files via upload

---
+++ Задача 6.cs
@@ -0,0 +1,12 @@
+﻿    Console.WriteLine("Введите число :");
+    string s1 = Console.ReadLine();
+    int Number1 = int.Parse(s1);
+        
+        if (Number1 % 2 > 0)
+        {
+            Console.WriteLine("Вы ввели нечетное число");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели четное число");
+        }
